1ed10d4c0d003e0cf2ea7f16989a5fd634e739ab
William Cheung <william.cheung@pb.com>
2020-04-30 16:55:34 -0400

    Rewrite to convert a single CSV file to an Excel format specified by the user. Remove bin directory since binaries not regenerated.

--- src/csv-to-xlsx.js
+++ src/csv-to-xlsx.js
@@ -23,54 +23,44 @@ if (module.parent) {
    *********************************************************/
   program
     .version(pkg.version, '-v, --version')
-    .option('-i, --input-dir [dir]', 'Input directory that has the CSV files', 'csv')
-    .option('-o, --output-dir [dir]', 'Output directory for the XLSX files', 'xlsx');
+    .option('-i, --input [file]', 'Input CSV file path')
+    .option('-o, --outputFormat ["xlsb", "xlsx", etc.]', 'Output Excel file format', 'xlsb');
 
   program.on('--help', function () {
     console.log(``);
-    console.log(`Created by: ${pkg.author.name}`);
-    console.log(`Please report bugs at: ${pkg.bugs.url}`);
-    console.log(`Version: ${pkg.version}`);
+    console.log(`Based on original version by: ${pkg.author.name}`);
+    console.log(`Original version: ${pkg.version}`);
   });
 
   program.parse(process.argv);
 
-  const csvPath = path.join(process.cwd(), program.inputDir);
-  const xlsxPath = path.join(process.cwd(), program.outputDir);
+  const csvPath = path.normalize(program.input);
 
   // check the csvPath
-  if (!fs.existsSync(csvPath)) {
-    // csv folder doesn't exist, doing it wrong
-    console.error(`Invalid input directory: ${csvPath}\n`);
+  if (!fs.existsSync(csvPath) || fs.lstatSync(csvPath).isDirectory()) {
+    // csv file doesn't exist or is a directory, doing it wrong
+    console.error(`Invalid input file: ${csvPath}\n`);
     process.exitCode = 1;
     program.help(); // exit immediately
   }
 
-  // check the xlsxPath
-  if (!fs.existsSync(xlsxPath)) {
-    // create xlsx folder
-    console.info(`Creating output directory: ${xlsxPath}`);
-    fs.mkdirpSync(xlsxPath);
+  // parse file
+  const fileObject = path.parse(csvPath);
+
+  // delete output file if exists
+  const outputFormat = program.outputFormat;
+  const outputPath = path.join(path.dirname(csvPath), `${fileObject.name}.${outputFormat}`);
+  if (fs.existsSync(outputPath)) {
+    console.warn(`Deleting existing output file: ${outputPath}`);
+    fs.unlinkSync(outputPath);
   }
 
-  // read csvPath
-  const csvFiles = fs.readdirSync(csvPath);
-
-  for (const file of csvFiles) {
-    // parse file
-    const fileObject = path.parse(file);
-    // check file extension
-    if (fileObject.ext !== '.csv') {
-      continue;
-    }
-    console.info(`Converting: ${fileObject.name}`);
-    // convert
-    try {
-      convertCsvToXlsx(path.join(csvPath, file), path.join(xlsxPath, `${fileObject.name}.xlsx`));
-    } catch (e) {
-      console.info(`${e.toString()}`);
-    }
+  // convert
+  console.info(`Converting: ${csvPath}`);
+  try {
+    convertCsvToXlsx(csvPath, outputPath);
+    console.info(`Created: ${outputPath}`);
+  } catch (e) {
+    console.info(`${e.toString()}`);
   }
-
-  console.info(`Complete.`);
 }
